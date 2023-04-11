398c928f56f87125a220d48e423b4056b6f1eb09
benjaminliang2 <benjaminliang2@gmail.com>
2023-04-10 15:52:58 -1000

    add pipe class

--- flappy_bird.py
+++ flappy_bird.py
@@ -12,7 +12,6 @@ BIRD_IMGS = [
              pygame.transform.scale2x(pygame.image.load(os.path.join( "imgs", "bird2.png"))),
              pygame.transform.scale2x(pygame.image.load(os.path.join( "imgs", "bird3.png")))
             ]
-PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join( "imgs", "pipe.png")))
 BASE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join( "imgs", "base.png")))
 BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join( "imgs", "bg.png")))
 
